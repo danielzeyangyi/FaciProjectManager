c91dd33edd8a26e1286a9966922f7a9a452d4da8
Zeyang Yi <danielzeyangyi@gmail.com>
2019-03-24 00:58:03 -0400

    Material 介绍

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoreModule } from './core/core.module'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+import { Optional, SkipSelf, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent
+  ]
+})
+export class CoreModule { 
+  constructor(@Optional() @SkipSelf() parent: CoreModule){
+    if(parent){
+      throw new Error('Core module is alrady loaded !!!');
+    }
+  }
+}
